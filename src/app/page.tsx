621128952117d6ced0eafc0bd98a6e70df4fa461
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-07-12 16:28:17 -0300

    feat: redux implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { AppDispatch, RootState } from "@/store";
+import { fetchUsersRequest } from "@/store/users/actions";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 export default function Home() {
+  const dispatch = useDispatch<AppDispatch>();
+  const { users, loading, error } = useSelector(
+    (state: RootState) => state.users
+  );
+
+  useEffect(() => {
+    dispatch(fetchUsersRequest());
+  }, [dispatch]);
+
   return <div>hello world</div>;
 }
