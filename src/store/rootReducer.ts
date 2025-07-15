9a888a5084a7ba45e18f5bd402b6215842b7e7dc
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-07-15 11:35:14 -0300

    feat: new saga added to store

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./users/reducer";
+import favoritesReducer from "./favorites/reducer";
 
 const rootReducer = combineReducers({
   users: userReducer,
+  favorites: favoritesReducer,
 });
 
 export default rootReducer;
--- src/store/rootSaga.ts
+++ src/store/rootSaga.ts
@@ -1,10 +1,11 @@
 import { all, AllEffect, fork, ForkEffect } from "redux-saga/effects";
 import { userSaga } from "./users/sagas";
+import { favoritesSaga } from "./favorites/sagas";
 
 export default function* rootSaga(): Generator<
   AllEffect<ForkEffect>,
   void,
   unknown
 > {
-  yield all([fork(userSaga)]);
+  yield all([fork(userSaga), fork(favoritesSaga)]);
 }
