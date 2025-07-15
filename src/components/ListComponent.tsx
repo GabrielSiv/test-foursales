4a09f9e043d136d5ce83643da70162728cfe4bba
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-07-15 16:29:26 -0300

    feat: list component container

---
+++ src/components/ListComponent.tsx
@@ -0,0 +1,58 @@
+import { StyledList } from "@/lib/styles/ListComponent";
+import { AppDispatch, RootState } from "@/store";
+import { fetchUsersRequest } from "@/store/users/actions";
+import React, { useCallback, useEffect, useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import UserAccordionItem from "./UserAccordionItem";
+import {
+  addFavorite,
+  loadFavoritesRequest,
+  removeFavorite,
+} from "@/store/favorites/actions";
+import { User } from "@/store/users/types";
+
+const ListComponent = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { users, loading } = useSelector((state: RootState) => state.users);
+  const favoriteUsers = useSelector(
+    (state: RootState) => state.favorites.users
+  );
+
+  const favoriteUserIds = useMemo(() => {
+    return new Set(favoriteUsers.map((user) => user.id));
+  }, [favoriteUsers]);
+
+  const handleToggleFavorite = useCallback(
+    (user: User) => {
+      const isFavorite = favoriteUserIds.has(user.id);
+      if (isFavorite) {
+        dispatch(removeFavorite(user.id));
+      } else {
+        dispatch(addFavorite(user));
+      }
+    },
+    [dispatch, favoriteUserIds]
+  );
+
+  useEffect(() => {
+    dispatch(fetchUsersRequest());
+    dispatch(loadFavoritesRequest());
+  }, []);
+  return (
+    <StyledList>
+      {users.map((user) => {
+        const isFavorite = favoriteUserIds.has(user.id);
+        return (
+          <UserAccordionItem
+            key={user.id}
+            isFavorite={isFavorite}
+            onToggleFavorite={handleToggleFavorite}
+            user={user}
+          />
+        );
+      })}
+    </StyledList>
+  );
+};
+
+export default ListComponent;
---
+++ src/lib/styles/ListComponent.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledList = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  background-color: #f8f8f8;
+  border-radius: 8px;
+  margin: 20px auto;
+`;
