0392e38bdd2f542847a791719ca9f0080954b468
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-07-16 10:32:38 -0300

    fix: home icon moved to icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,23 +13,7 @@ import {
 } from "@/lib/styles/Header";
 
 import { setSearchTerm, setFilterType } from "@/store/filter/actions";
-
-const HomeIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="32"
-    height="32"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-  >
-    <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-    <polyline points="9 22 9 12 15 12 15 22" />
-  </svg>
-);
+import { HomeIcon } from "@/lib/Icons/HomeIcon";
 
 export default function Header() {
   const router = useRouter();
