4d9c233dba09741e09d6dc8e321bb7813f2495db
Gabriel Silvino <gabriel.siv720@gmail.com>
2025-07-16 13:06:33 -0300

    fix: user accordion oo

--- src/components/UserAccordionItem.tsx
+++ src/components/UserAccordionItem.tsx
@@ -18,56 +18,74 @@ interface AccordionItemProps {
   onToggleFavorite: (user: User) => void;
 }
 
-const UserAccordionItem = ({
-  user,
-  isFavorite,
-  onToggleFavorite,
-}: AccordionItemProps) => {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const btnText = isOpen ? "VER MENOS" : "VER MAIS";
-  return (
-    <AccordionItemContainer $isOpen={isOpen}>
-      <AccordionHeader>
-        <h3>{user.name}</h3>
-        <p>{user.email}</p>
-        <p>{user.phone}</p>
-        <FavoriteButton
-          $isFavorite={isFavorite}
-          onClick={(e) => {
-            e.stopPropagation();
-            onToggleFavorite(user);
-          }}
-        >
-          {isFavorite ? "Remover" : "Favoritar"}
-        </FavoriteButton>
-      </AccordionHeader>
-      <AccordionContent $isOpen={isOpen}>
-        <p>
-          <strong>Endereço:</strong>
-          <br />
-          {user.address.street},{user.address.suite}, {user.address.city},{" "}
-          {user.address.zipcode}
-        </p>
-        <p>
-          <strong>Empresa:</strong>
-          <br />
-          {user.company.name}, "{user.company.catchPhrase}"
-        </p>
-        <p>
-          <a href={`/users/${user.id}`} rel="noopener noreferrer">
-            <strong>Mais detalhes</strong>
-          </a>
-        </p>
-      </AccordionContent>
-      <ShowMoreButton
-        onClick={() => {
-          setIsOpen((prev) => !prev);
-        }}
-      >
-        {btnText}
-      </ShowMoreButton>
-    </AccordionItemContainer>
-  );
-};
+interface AccordionItemState {
+  isOpen: boolean;
+}
+
+class UserAccordionItem extends React.Component<
+  AccordionItemProps,
+  AccordionItemState
+> {
+  constructor(props: AccordionItemProps) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  toggleAccordion = () => {
+    this.setState((prevState) => ({
+      isOpen: !prevState.isOpen,
+    }));
+  };
+
+  render() {
+    const { user, isFavorite, onToggleFavorite } = this.props;
+
+    const { isOpen } = this.state;
+
+    const btnText = isOpen ? "VER MENOS" : "VER MAIS";
+
+    return (
+      <AccordionItemContainer $isOpen={isOpen}>
+        <AccordionHeader>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
+          <p>{user.phone}</p>
+          <FavoriteButton
+            $isFavorite={isFavorite}
+            onClick={(e) => {
+              e.stopPropagation();
+              onToggleFavorite(user);
+            }}
+          >
+            {isFavorite ? "Remover" : "Favoritar"}
+          </FavoriteButton>
+        </AccordionHeader>
+        <AccordionContent $isOpen={isOpen}>
+          <p>
+            <strong>Endereço:</strong>
+            <br />
+            {user.address.street},{user.address.suite}, {user.address.city},{" "}
+            {user.address.zipcode}
+          </p>
+          <p>
+            <strong>Empresa:</strong>
+            <br />
+            {user.company.name}, "{user.company.catchPhrase}"
+          </p>
+          <p>
+            <a href={`/users/${user.id}`} rel="noopener noreferrer">
+              <strong>Mais detalhes</strong>
+            </a>
+          </p>
+        </AccordionContent>
+        <ShowMoreButton onClick={this.toggleAccordion}>
+          {btnText}
+        </ShowMoreButton>
+      </AccordionItemContainer>
+    );
+  }
+}
 
 export default UserAccordionItem;
